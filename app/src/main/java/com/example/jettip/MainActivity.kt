633ec96710d944efbf9614714e245f599b8f1442
Hossein <hhasnabadi@yahoo.com>
2024-08-07 18:52:13 +0330

    jettip

--- app/src/main/java/com/example/jettip/MainActivity.kt
+++ app/src/main/java/com/example/jettip/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.jettip
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -29,6 +37,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jettip.components.InputField
 import com.example.jettip.ui.theme.JetTipTheme
+import com.example.jettip.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,8 +46,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetTipTheme {
                 MyApp {
-                    //TopHeader()
+                  //  TopHeader()
                     MainContent()
+
+
 
                 }
 
@@ -46,79 +57,139 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
-fun MyApp(content:@Composable () -> Unit){
+fun MyApp(content: @Composable () -> Unit) {
     Surface(color = MaterialTheme.colorScheme.background) {
-        MainContent()
+
+        Column(modifier = Modifier.padding(12.dp)) {
+            MainContent()
+
+
+
+        }
 
 
     }
 
 }
+
 //@Preview
 @Composable
-fun TopHeader(totalPerPerson:Double = 132.0){
-    Surface (modifier = Modifier
-        .fillMaxWidth()
-        .height(150.dp)
-        .padding(8.dp)
-        .clip(shape = RoundedCornerShape(corner = CornerSize(2.dp)))
-        , color = Color(0xFFE9D7F7)
-    ){
-        Column(modifier = Modifier
-            .padding(12.dp)
-            , verticalArrangement = Arrangement.Center
-            , horizontalAlignment = Alignment.CenterHorizontally
+fun TopHeader(totalPerPerson: Double = 132.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(8.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(2.dp))), color = Color(0xFFE9D7F7)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(12.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             val total = "%.2f".format(totalPerPerson)
-            Text(text = "Total Per Person",
+            Text(
+                text = "Total Per Person",
                 //h5
-                style =MaterialTheme.typography.headlineSmall)
-            Text(text = "$$total",
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Text(
+                text = "$$total",
                 //H4
                 style = MaterialTheme.typography.displayMedium,
-                fontWeight = FontWeight.ExtraBold)
+                fontWeight = FontWeight.ExtraBold
+            )
         }
     }
 }
 
 @Preview
 @Composable
-fun MainContent(){
+fun MainContent() {
+
+        BillFrom() {}
+
+
+    }
+
+
+
+@Preview
+@Composable
+fun BillFrom(
+    modifier: Modifier = Modifier,
+    onValChange: (String) -> Unit = {},
+) {
     val totalBillState = remember {
         mutableStateOf("")
     }
-    val validState = remember(totalBillState) {
+    val validState = remember(totalBillState.value) {
         totalBillState.value.trim().isNotEmpty()
     }
 
     val keyboardController = LocalSoftwareKeyboardController.current
-    Surface(modifier = Modifier
-        .padding(20.dp)
-        .fillMaxWidth(), shape = RoundedCornerShape(corner = CornerSize(8.dp))
-        , border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    Surface(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
 
     ) {
-        Column {
-            InputField(valueState =totalBillState
-                , labelId ="Enter Bill",
-                enabled =true,
-                isSingleLine =true
-
-                , onAction = KeyboardActions{
-                    if (!validState)return@KeyboardActions
-                    //Todo onvaluechanged
+        Column(modifier = Modifier.padding(6.dp),
+           verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ){
+            InputField(valueState = totalBillState
+                , labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true, onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
                     keyboardController?.hide()
 
+                })
+
+            if (validState) {
+                Row(
+                    modifier = Modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start
+                )
+                {
+                    Text(
+                        text = "Split",
+                        modifier = Modifier.align(
+                            alignment = Alignment.CenterVertically
+                        )
+                    )
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row (modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End){
+                        RoundIconButton(
+
+
+
+                            imageVector =Icons.Default.Remove,
+                            onClick = {})
+
+                        RoundIconButton(
+                            imageVector =Icons.Default.Add,
+                            onClick = { })
+
+
+                    }
                 }
-
-
-            )
-
-
+            } else {
+                Box() {}
+            }
         }
 
+
     }
+
 }
 
 
